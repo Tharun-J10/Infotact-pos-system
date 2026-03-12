dc1583ee966a735bb5434c81192ad9b1696f28f2
jahnavi4006 <jahnavi4006@gmail.com>
2026-03-12 22:26:56 +0530

    modified server.js

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("Backend running successfully");
+  res.send("POS Backend running successfully");
 });
 
 app.listen(5000, () => {
